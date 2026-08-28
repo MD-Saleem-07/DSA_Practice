f5268470b432e17823c9b0c2cf5ef9e8c40eb974
Md Saleem <87407229+MD-Saleem-07@users.noreply.github.com>
2026-08-28 20:26:28 +0530

    Time: 18 ms (77.68%), Space: 108.2 MB (47.1%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,35 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    // public boolean containsDuplicate(int[] nums) {
      
-     Arrays.sort(nums);
-    int j=0;
-    for(int i=1;i<nums.length;i++)
-    {
-        if(nums[i]==nums[j])
+    //  Arrays.sort(nums);
+    // int j=0;
+    // for(int i=1;i<nums.length;i++)
+    // {
+    //     if(nums[i]==nums[j])
+    //     {
+    //         return true;
+    //     }
+    //     j++;
+    // }
+    // return false;
+
+    // }
+
+      public boolean containsDuplicate(int[] nums) {
+     
+        Set<Integer> set= new HashSet<>();
+
+        for(int num : nums)
         {
-            return true;
+            if(set.contains(num))
+            {
+                return true;
+            }
+
+            set.add(num);
         }
-        j++;
-    }
-    return false;
+        return false;
+
 
     }
 }
